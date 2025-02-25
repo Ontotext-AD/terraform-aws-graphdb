f57b00e459ab55fd6d3b9401bc21d876a5628b8a
Mihail Radkov <mihail.radkov@ontotext.com>
2025-02-25 10:51:33 +0200

    Update GraphDB version to 10.8.4
    Updated the default GraphDB version to the latest patch version.

--- variables.tf
+++ variables.tf
@@ -229,7 +229,7 @@ variable "ami_id" {
 variable "graphdb_version" {
   description = "GraphDB version"
   type        = string
-  default     = "10.8.3"
+  default     = "10.8.4"
   nullable    = false
 }
 
